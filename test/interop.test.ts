76184cf468d14bf2903e614ee0113ef437be1af0
Brian Richter <brian@aviary.tech>
2025-01-22 20:14:34 -0800

    added gist DID

--- test/interop.test.ts
+++ test/interop.test.ts
@@ -30,9 +30,10 @@ describe("did:webvh interoperability tests", async () => {
     expect(meta.portable).toBe(false);
   })
 
-  test.skip("gist", async () => {
-    const did = "did:webvh:QmbnQXj7DhWFrmgjDPKZCybn8fkKW7Wze57SQHpwsSQ7NZ:gist.githubusercontent.com:brianorwhatever:9c4633d18eb644f7a47f93a802691626:raw";
+  test("gist", async () => {
+    const did = "did:webvh:QmPEQVM1JPTyrvEgBcDXwjK4TeyLGSX1PxjgyeAisdWM1p:gist.githubusercontent.com:brianorwhatever:9c4633d18eb644f7a47f93a802691626:raw";
     const {did: resolvedDID, meta} = await resolveDID(did);
+
     expect(resolvedDID).toBe(did);
     expect(meta.prerotation).toBe(false);
     expect(meta.portable).toBe(false);

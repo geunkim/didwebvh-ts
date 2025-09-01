70ee446c9192128eabd084c8b278783e7363a7fd
Ariel Gentile <gentilester@gmail.com>
2025-09-01 18:12:50 -0300

    fix: remove extra comma
    Signed-off-by: Ariel Gentile <gentilester@gmail.com>

--- test/utils.ts
+++ test/utils.ts
@@ -70,7 +70,7 @@ export async function generateTestVerificationMethod(purpose: "authentication" |
     type: 'Multikey',
     publicKeyMultibase: publicKey,
     secretKeyMultibase: secretKey,
-    purpose,
+    purpose
   };
 }
 
